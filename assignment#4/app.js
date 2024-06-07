d5d842bafe483728ca7bd57f183332d38fecf72e
Hina-Aamir <hinaaamir9911@gmail.com>
2024-06-07 21:50:34 +0500

    first commit assignment

---
+++ assignment#4/app.js
@@ -0,0 +1,90 @@
+
+
+//1.ASCII CODES
+// var character=prompt("ENTER ANY  CHARACTER");
+// var result=character.charCodeAt();
+// if(result>=65&&result<=90){
+//     document.write("THE GIVEN CHARACTER IS UPPERCASE");
+// }
+// else if(result>=97&&result<=122){
+//     document.write("the given charcter is lowercase");
+// }
+// else  if (result>=48&result<=57){
+//     document.write("THE GIVEN CHARCTER IS NUMBER")
+// }
+
+
+//2.accept two integers
+
+// var int1=+prompt("ENTER ANY FIRST INTGERS")
+// var int2=+prompt("ENTER ANY SECOND INTEGERS")
+
+// if( int1<int2){
+//     document.write("THE SECOND INETGERS "+int2+" is larger one")
+// }
+// else if(int1>int2){
+//     document.write("THE FIRST INTEGERS "+int1+" is larger one")
+// }
+// else if (int1==int2){
+//     document.write("the both integers are equal")
+// }
+
+
+//3.input number as positive negative zero
+
+
+// var num=+prompt("ENTER ANY NUMBER")
+// if(num>0){
+//     document.write("THE INPUT NUMBER IS POSITIVE")
+
+// }
+//  else if(num<0){
+//     document.write("THE INPUT NUMBER IS NEGATIVE")
+//  }
+
+// else if(num==0){
+//     document.write("THE INPUT NUMBER IS EQUAL TO ZERO")
+// }
+
+
+// 4. TAKE INPUT CHRACTER RETURN VOWEL
+// var bol
+// var char=prompt("ENTER  STRING OF LENGTH 1")
+// if(char=="a"||char=="e"||char=="e"||char=="i"||char=="o"||char=="u"){
+//     bol=true;
+//     document.write("TRUE<BR>")
+//     document.write("the given character is vowel")
+// }
+// else {
+// bol=false;
+//     document.write("FALSE")
+// }
+
+//6.
+
+// var greeting
+// var hour =+prompt("ENTER THE HOUR IN 24 HOUR format")
+// if (hour <=24){
+// if(hour>=0&&hour<=6)
+//     {
+//         greeting="midnight"
+//     }
+//     if(hour>6&&hour<=11){
+//         greeting="good day"
+//     }
+//     if(hour>11&&hour<=16){
+//         greeting="good afternoon"
+//     }
+//     if (hour>16&&hour<20){
+//         greeting="good evening"
+//     }
+//     if(hour>=20&&hour<=24){
+//         greeting="good night"
+//     }
+// }
+// else{
+//     greeting="wrong format does not exist"
+// }
+// document.write(greeting)
+
+
